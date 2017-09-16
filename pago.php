f3721293b2639da25df67edcc5aa43f805068d1d
mpandolfelli <martinpandolfelli@gmail.com>
2017-09-16 15:21:31 -0300

    mercado pago

--- pago.php
+++ pago.php
@@ -1,8 +1,11 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once ('./lib/mercadopago.php');
 
 if($_POST){
-	$url = 'http://codicilabs.com/trabajos/chefnity/'
+
+	$url = 'http://codicilabs.com/trabajos/chefnity/';
 	$mp = new MP ("2265501374633304", "0dFWJNE1CqTT91acmo8YUfGXasg3wUNy");
 
 	$access_token = $mp->get_access_token();
@@ -13,17 +16,20 @@ if($_POST){
 		array(
 		    "id" => $_POST['id'],
 		    "title" => $_POST['title'],
-		    "quantity" => $_POST['quantity'],
+		    "quantity" => intval($_POST['amount']),
 		    "currency_id" => "ARS", //Available currencies at: https://api.mercadopago.com/currencies
 		    "picture_url" => $url.$_POST['image'],
-		    "unit_price" => $_POST['price']
+		    "unit_price" => intval($_POST['price'])
 		)
 	    )
 	);
 
 	$preference = $mp->create_preference($preference_data);
 	if($preference){
-		header($preference['response']['sandbox_init_point']);
+		$redirect = $preference['response']['sandbox_init_point'];
+		header('Location : http://codicilabs.com');
+		//echo $redirect;
+		exit;
 	}else{
 		die('Error');
 	}
