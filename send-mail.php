1ffc517bd6452f5eb6e5337f457d0e1b600e5c77
gdzabaleta <gdzabaleta@gmail.com>
2017-10-21 14:47:04 -0300

    agregando la parte de enviar mail

---
+++ send-mail.php
@@ -0,0 +1,19 @@
+<?php
+
+$type=$_POST['type'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+$phone=$_POST['phone'];
+    
+    $body .= "Chef/Comprador: " . $type . "\n"; 
+    $body .= "Nombre: " . $name . "\n"; 
+    $body .= "Email: " . $email . "\n"; 
+	$body .= "Teléfono: " . $phone . "\n"; 
+    $body .= "Mensaje: " . $message . "\n"; 
+
+    //replace with your email-sotomayorsebastian4@gmail.com
+    mail("gdzabaleta@gmail.com","Nuevo email","$body","From: $Email"); 
+
+  
+?>
